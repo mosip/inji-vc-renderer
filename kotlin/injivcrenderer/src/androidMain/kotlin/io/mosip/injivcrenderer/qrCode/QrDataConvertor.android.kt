4482cca99ede0c08cee536fff8b2a5c51d9730d2
balachandarg-tw <balachandar.g@thoughtworks.com>
2025-09-09 13:56:33 +0530

    [INJIMOB-3497]: QR code fallback handling
    Signed-off-by: balachandarg-tw <balachandar.g@thoughtworks.com>

--- kotlin/injivcrenderer/src/androidMain/kotlin/io/mosip/injivcrenderer/qrCode/QrDataConvertor.android.kt
+++ kotlin/injivcrenderer/src/androidMain/kotlin/io/mosip/injivcrenderer/qrCode/QrDataConvertor.android.kt
@@ -1,16 +1,9 @@
 package io.mosip.injivcrenderer.qrCode
 
-import android.util.Log
 import io.mosip.pixelpass.PixelPass
 
 
 actual fun convertQrDataIntoBase64(qrData: String): String {
-    try {
         val pixelPass = PixelPass()
         return pixelPass.generateQRCode(qrData)
-    } catch (e: Exception){
-        Log.d("Error occurred while converting Qr Data to Base64 String::", e.toString())
-        e.printStackTrace()
-        return ""
-    }
 }
--- kotlin/injivcrenderer/src/jvmMain/kotlin/io/mosip/injivcrenderer/qrCode/QrDataConvertor.jvm.kt
+++ kotlin/injivcrenderer/src/jvmMain/kotlin/io/mosip/injivcrenderer/qrCode/QrDataConvertor.jvm.kt
@@ -4,13 +4,6 @@ import io.mosip.pixelpass.PixelPass
 
 
 actual fun convertQrDataIntoBase64(qrData: String): String {
-    try {
-        val pixelPass = PixelPass()
-        return pixelPass.generateQRCode(qrData)
-    }
-    catch (e: Exception){
-        println("Error occurred while converting Qr Data to Base64 String::$e")
-        e.printStackTrace()
-        return ""
-    }
+    val pixelPass = PixelPass()
+    return pixelPass.generateQRCode(qrData)
 }
